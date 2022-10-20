ab71d70edd1772411c18944877b3dde7e70cb6fe
Elena.T <lena_tulguk@mail.ru>
2022-10-20 23:37:44 +0800

    les4 ex2 done

--- les4_ex1.py
+++ les4_ex1.py
@@ -1,6 +1,17 @@
 # задача 1. Задайте натуральное число N. Напишите программу, которая составит список простых множителей числа N.
 
 
+def get_number():
+    try:
+        a = int(input('Enter a number\n'))
+        if a < 1:
+            print('Enter a valid number')
+        else:
+            return a
+    except:
+        print('Enter a valid number')
+
+
 def is_prime(numb):
     if numb % 2 == 0:
         return numb == 2
@@ -13,7 +24,7 @@ def is_prime(numb):
 def look_for_num(number):
     mult_list = []
     while number > 1:
-        for i in range(2, num+1):
+        for i in range(2, num + 1):
             if number % i == 0:
                 if is_prime(i):
                     mult_list.append(i)
@@ -22,7 +33,16 @@ def look_for_num(number):
     return mult_list
 
 
-num = int(input('Enter a number'))
-mults = look_for_num(num)
-print(mults)
 
+do = True
+while do:
+    num = None
+    while num is None:
+        num = get_number()
+    mults = look_for_num(num)
+    print(mults)
+    print("Again? Press 'N' to exit, press any other key to continue")
+    want = input()
+    if want == 'N' or want == 'n':
+
+        do = False
