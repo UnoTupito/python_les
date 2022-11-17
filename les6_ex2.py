26317548107ebd1b43d683ea76a649cac3e17b57
Elena.T <lena_tulguk@mail.ru>
2022-11-17 15:28:34 +0800

    les6 ex2  finished with comments

--- les6_ex2.py
+++ les6_ex2.py
@@ -1,23 +1,27 @@
 # Напишите программу вычисления арифметического выражения заданного строкой.
 # Используйте операции +,-,/,*. приоритет операций стандартный.
+
 # кажется, я решила очень перегруженно и сложно. врял ли эта задача пополнит золотой фонд моих кодов,
 # но я тем не менее рада, что сделала её сама, хотя изначально собиралась вообще не решать из-за сложности
+
 # программа не работает со скобками и степенями
 
 import re
 
 
 def get_mult(tsk):
-    mult_pattern = r'\d+\.?\d*\*\d+\.?\d*'
+    mult_pattern = r'\d+\.?\d*\*\d+\.?\d*' # ищем паатерн: два числа(возможно, с точкой) и между ними знак умножения
     part = (re.search(mult_pattern, tsk)).group(0)
-    a = float(part[:part.find('*')])
-    b = float(part[part.find('*') + 1:])
+    a = float(part[:part.find('*')])  # первый множитель
+    b = float(part[part.find('*') + 1:])  # второй ножитель
     answ = str(a * b)
+    # собираем пример обратно, вставив вместо части с умножением результат этого умножения:
     tsk = tsk[0:tsk.find(part)] + answ + tsk[tsk.find(part) + len(part):]
     return tsk
 
 
 def get_div(tsk):
+    # такая же логгика, как у умножения, только без промежуточных переменных
     div_pattern = r'\d+\.?\d*\/\d+\.?\d*'
     part = (re.search(div_pattern, tsk)).group(0)
     answ = str(float(part[:part.find('/')]) / float(part[part.find('/') + 1:]))
@@ -36,6 +40,7 @@ def get_sum(tsk):
 def get_dif(tsk):
     dif_pattern = r'\-?\d+\.?\d*\-\d+\.?\d*'
     part = (re.search(dif_pattern, tsk)).group(0)
+    # разветвление на случай, если первое число отрицательное (то есть нужно считать от второго символа минуса)
     if tsk[0] == '-':
         answ = str(float(part[:part.find('-', 1)]) - float(part[part.find('-', 1) + 1:]))
     else:
@@ -46,9 +51,9 @@ def get_dif(tsk):
 
 def calc(tsk):
     float_pattern = r'^\-?\d+\.?\d*$'
-    if re.match(float_pattern, tsk):
+    if re.match(float_pattern, tsk): # если у нас просто число, возвращаем его
         return tsk
-    elif '*' in tsk and '/' in tsk:
+    elif '*' in tsk and '/' in tsk:  # сначала деление или умножение. Начинаем с того, что левее
         if tsk.find('*') < tsk.find('/'):
             tsk = get_mult(tsk)
             tsk = calc(tsk)
@@ -65,7 +70,7 @@ def calc(tsk):
         tsk = get_div(tsk)
         tsk = calc(tsk)
         return tsk
-    elif '+' in tsk and '-' in tsk:
+    elif '+' in tsk and '-' in tsk:  # начинаем с той операции, которая левее
         if tsk.find('+') < tsk.find('-'):
             tsk = get_sum(tsk)
             tsk = calc(tsk)
